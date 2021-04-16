d5e34851d692ce5b38602698629d5da8a9a1b4a2
meowphanh <82545771+meowphanh@users.noreply.github.com>
2021-04-17 03:18:23 +0700

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -71,38 +71,25 @@ def get_readable_time(seconds: int) -> str:
 
 
 PM_START_TEXT = """
-Hey hi {}, I'm {}!
-I am an Anime themed group management bot.
-Built by weebs for weebs, I specialize in managing anime eccentric communities!
+Chào {}, Mình là {}!
+Mình là người quản lý group trên Telegram!
+Mình được tạo ra bởi chị chủ @phuonganh_lnb
+Rất vui vì được giúp đỡ bạn 🥰🥰🥰
 """
 
 HELP_STRINGS = """
-Hey there! My name is *{}*.
-I'm a Hero For Fun and help admins manage their groups with One Punch! Have a look at the following for an idea of some of \
-the things I can help you with.
-
-*Main* commands available:
- • /help: PM's you this message.
- • /help <module name>: PM's you info about that module.
- • /donate: information on how to donate!
- • /settings:
-   • in PM: will send you your settings for all supported modules.
-   • in a group: will redirect you to pm, with all that chat's settings.
-
-
-{}
-And the following:
+Hãy chọn một cái module bên dưới để mình có thể hỗ trợ bạn nhé!
 """.format(
     dispatcher.bot.first_name,
     "" if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n",
 )
 
-SAITAMA_IMG = "https://telegra.ph/file/46e6d9dfcb3eb9eae95d9.jpg"
+SAITAMA_IMG = "https://danviet.mediacdn.vn/zoom/700_438/2020/11/19/le-bong-1a-1605770106876600963044-crop-1606384717151357706734.jpg"
 
-DONATE_STRING = """Heya, glad to hear you want to donate!
- You can support the project via [Paypal](ko-fi.com/sawada) or by contacting @Sawada \
- Supporting isnt always financial! \
- Those who cannot provide monetary support are welcome to help us develop the bot at @OnePunchDev."""
+DONATE_STRING = """Heya, bạn muốn ủng hộ cho chị chủ hả?
+ Bạn có thể donate cho chị chủ qua [Paypal](https://www.paypal.me/laungungbitch) nhé!
+ 😍😍😍
+ """
 
 IMPORTED = {}
 MIGRATEABLE = []
