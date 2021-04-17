551398ca5d12f67f73eeaecc0ebb13e08e0b114f
meowphanh <82545771+meowphanh@users.noreply.github.com>
2021-04-18 03:59:17 +0700

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -207,6 +207,20 @@ def start(update: Update, context: CallbackContext):
                 ),
                 parse_mode=ParseMode.MARKDOWN,
                 disable_web_page_preview=True,
+                buttons = [
+    [
+        InlineKeyboardButton(text="🚀 INFO 🚀", callback_data="aboutmanu_"),
+    ],
+    [
+        InlineKeyboardButton(text="❓ Help & Commands ❓", callback_data="help_back"),
+    ],
+    [
+        InlineKeyboardButton(
+            text="💫 Add LEGEND BOT to your group 💫",
+            url="t.me/grand50_bot?startgroup=true",
+        ),
+    ],
+]
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
@@ -217,7 +231,6 @@ def start(update: Update, context: CallbackContext):
                                 ),
                             ),
                         ],
-                        [InlineKeyboardButton(text="help", callback_data="donate_back")],
                         [
                             InlineKeyboardButton(
                                 text="😎 Trợ Giúp",
