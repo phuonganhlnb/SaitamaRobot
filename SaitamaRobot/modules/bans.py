15babec0e5a8833334e85d77d7492873ae7f4c71
phuonganhlnb <82063045+phuonganhlnb@users.noreply.github.com>
2021-04-19 10:10:12 +0700

    Update bans.py

--- SaitamaRobot/modules/bans.py
+++ SaitamaRobot/modules/bans.py
@@ -49,40 +49,40 @@ def ban(update: Update, context: CallbackContext) -> str:
     user_id, reason = extract_user_and_text(message, args)
 
     if not user_id:
-        message.reply_text("I doubt that's a user.")
+        message.reply_text("Mình nghi ngờ đó là một người dùng.")
         return log_message
     try:
         member = chat.get_member(user_id)
     except BadRequest as excp:
-        if excp.message != "User not found":
+        if excp.message != "Không tìm thấy User":
             raise
-        message.reply_text("Can't seem to find this person.")
+        message.reply_text("Dường như không thể tìm thấy người này.")
         return log_message
     if user_id == bot.id:
-        message.reply_text("Oh yeah, ban myself, noob!")
+        message.reply_text("Oh yeah, tự ban chính mình à, ngốc quá!")
         return log_message
 
     if is_user_ban_protected(chat, user_id, member) and user not in DEV_USERS:
         if user_id == OWNER_ID:
-            message.reply_text("Trying to put me against a God level disaster huh?")
+            message.reply_text("Cố gắng đưa mình đi chống lại một thảm họa cấp độ Chúa hả?")
         elif user_id in DEV_USERS:
-            message.reply_text("I can't act against our own.")
+            message.reply_text("Mình không thể hành động chống lại chính chị chủ của mình được.")
         elif user_id in DRAGONS:
             message.reply_text(
-                "Fighting this Dragon here will put civilian lives at risk.",
+                "Chiến đấu với con Rồng này ở đây sẽ khiến cuộc sống của dân thường gặp nguy hiểm.",
             )
         elif user_id in DEMONS:
             message.reply_text(
-                "Bring an order from Heroes association to fight a Demon disaster.",
+                "Mang lệnh từ hiệp hội Anh hùng để chống lại một thảm họa quỷ.",
             )
         elif user_id in TIGERS:
             message.reply_text(
-                "Bring an order from Heroes association to fight a Tiger disaster.",
+                "Mang lệnh từ hiệp hội anh hùng để chống lại thảm họa Tiger.",
             )
         elif user_id in WOLVES:
-            message.reply_text("Wolf abilities make them ban immune!")
+            message.reply_text("Khả năng của loài sói khiến chúng miễn nhiễm với lệnh cấm!")
         else:
-            message.reply_text("This user has immunity and cannot be banned.")
+            message.reply_text("Người dùng này có quyền miễn trừ và không thể bị cấm.")
         return log_message
     if message.text.startswith("/s"):
         silent = True
@@ -161,20 +161,20 @@ def temp_ban(update: Update, context: CallbackContext) -> str:
     try:
         member = chat.get_member(user_id)
     except BadRequest as excp:
-        if excp.message != "User not found":
+        if excp.message != "Người dùng không được tìm thấy":
             raise
-        message.reply_text("I can't seem to find this user.")
+        message.reply_text("Mình dường như không thể tìm thấy người dùng này.")
         return log_message
     if user_id == bot.id:
-        message.reply_text("I'm not gonna BAN myself, are you crazy?")
+        message.reply_text("Mình sẽ không BAN chính mình đâu, Haizzzz")
         return log_message
 
     if is_user_ban_protected(chat, user_id, member):
-        message.reply_text("I don't feel like it.")
+        message.reply_text("Mình không cảm thấy thích nó.")
         return log_message
 
     if not reason:
-        message.reply_text("You haven't specified a time to ban this user for!")
+        message.reply_text("Bạn chưa chỉ định thời gian để cấm người dùng này!")
         return log_message
 
     split_reason = reason.split(None, 1)
@@ -223,7 +223,7 @@ def temp_ban(update: Update, context: CallbackContext) -> str:
                 chat.id,
                 excp.message,
             )
-            message.reply_text("Well damn, I can't ban that user.")
+            message.reply_text("Hix, mình không thể cấm người dùng đó.")
 
     return log_message
 
@@ -244,23 +244,23 @@ def punch(update: Update, context: CallbackContext) -> str:
     user_id, reason = extract_user_and_text(message, args)
 
     if not user_id:
-        message.reply_text("I doubt that's a user.")
+        message.reply_text("Mình nghi ngờ đó là một người dùng.")
         return log_message
 
     try:
         member = chat.get_member(user_id)
     except BadRequest as excp:
-        if excp.message != "User not found":
+        if excp.message != "Không tìm thấy người dùng":
             raise
 
-        message.reply_text("I can't seem to find this user.")
+        message.reply_text("Mình dường như không thể tìm thấy người dùng này.")
         return log_message
     if user_id == bot.id:
-        message.reply_text("Yeahhh I'm not gonna do that.")
+        message.reply_text("Yeahhh!! Mình sẽ không làm điều đó.")
         return log_message
 
     if is_user_ban_protected(chat, user_id):
-        message.reply_text("I really wish I could punch this user....")
+        message.reply_text("Mình thực sự ước mình có thể đấm người dùng này ....")
         return log_message
 
     res = chat.unban_member(user_id)  # unban on current user = kick
@@ -268,7 +268,7 @@ def punch(update: Update, context: CallbackContext) -> str:
         # bot.send_sticker(chat.id, BAN_STICKER)  # banhammer marie sticker
         bot.sendMessage(
             chat.id,
-            f"One Punched! {mention_html(member.user.id, html.escape(member.user.first_name))}.",
+            f"Đã cho ăn đấm! {mention_html(member.user.id, html.escape(member.user.first_name))}.",
             parse_mode=ParseMode.HTML,
         )
         log = (
@@ -278,12 +278,12 @@ def punch(update: Update, context: CallbackContext) -> str:
             f"<b>User:</b> {mention_html(member.user.id, html.escape(member.user.first_name))}"
         )
         if reason:
-            log += f"\n<b>Reason:</b> {reason}"
+            log += f"\n<b>Lý do:</b> {reason}"
 
         return log
 
     else:
-        message.reply_text("Well damn, I can't punch that user.")
+        message.reply_text("Hix, mình không thể đấm người dùng đó.")
 
     return log_message
 
@@ -294,14 +294,14 @@ def punch(update: Update, context: CallbackContext) -> str:
 def punchme(update: Update, context: CallbackContext):
     user_id = update.effective_message.from_user.id
     if is_user_admin(update.effective_chat, user_id):
-        update.effective_message.reply_text("I wish I could... but you're an admin.")
+        update.effective_message.reply_text("Mình ước là mình có thể ... nhưng bạn là quản trị viên mà @@.")
         return
 
     res = update.effective_chat.unban_member(user_id)  # unban on current user = kick
     if res:
-        update.effective_message.reply_text("*punches you out of the group*")
+        update.effective_message.reply_text("*Đấm bạn ra khỏi nhóm*")
     else:
-        update.effective_message.reply_text("Huh? I can't :/")
+        update.effective_message.reply_text("Huh? mình không thể :/")
 
 
 @run_async
@@ -320,26 +320,26 @@ def unban(update: Update, context: CallbackContext) -> str:
     user_id, reason = extract_user_and_text(message, args)
 
     if not user_id:
-        message.reply_text("I doubt that's a user.")
+        message.reply_text("Mình nghi ngờ đó là một người dùng.")
         return log_message
 
     try:
         member = chat.get_member(user_id)
     except BadRequest as excp:
-        if excp.message != "User not found":
+        if excp.message != "Không tìm thấy người dùng này":
             raise
-        message.reply_text("I can't seem to find this user.")
+        message.reply_text("Mình dường như không thể tìm thấy người dùng này.")
         return log_message
     if user_id == bot.id:
-        message.reply_text("How would I unban myself if I wasn't here...?")
+        message.reply_text("Làm thế nào mình có thể tự bỏ cấm nếu như mình không có ở đây ...?")
         return log_message
 
     if is_user_in_chat(chat, user_id):
-        message.reply_text("Isn't this person already here??")
+        message.reply_text("Không phải người này đã ở đây sao??")
         return log_message
 
     chat.unban_member(user_id)
-    message.reply_text("Yep, this user can join!")
+    message.reply_text("Yep, người dùng này có thể tham gia!")
 
     log = (
         f"<b>{html.escape(chat.title)}:</b>\n"
@@ -348,7 +348,7 @@ def unban(update: Update, context: CallbackContext) -> str:
         f"<b>User:</b> {mention_html(member.user.id, html.escape(member.user.first_name))}"
     )
     if reason:
-        log += f"\n<b>Reason:</b> {reason}"
+        log += f"\n<b>Lý do:</b> {reason}"
 
     return log
 
@@ -368,7 +368,7 @@ def selfunban(context: CallbackContext, update: Update) -> str:
     try:
         chat_id = int(args[0])
     except:
-        message.reply_text("Give a valid chat ID.")
+        message.reply_text("Vui lòng cung cấp một ID chat hợp lệ.")
         return
 
     chat = bot.getChat(chat_id)
@@ -376,18 +376,18 @@ def selfunban(context: CallbackContext, update: Update) -> str:
     try:
         member = chat.get_member(user.id)
     except BadRequest as excp:
-        if excp.message == "User not found":
-            message.reply_text("I can't seem to find this user.")
+        if excp.message == "Không tìm thấy người dùng":
+            message.reply_text("Mình dường như không thể tìm thấy người dùng này.")
             return
         else:
             raise
 
     if is_user_in_chat(chat, user.id):
-        message.reply_text("Aren't you already in the chat??")
+        message.reply_text("Bạn vẫn chưa tham gia chat hả??")
         return
 
     chat.unban_member(user.id)
-    message.reply_text("Yep, I have unbanned you.")
+    message.reply_text("Yep, mình đã unban bạn.")
 
     log = (
         f"<b>{html.escape(chat.title)}:</b>\n"
@@ -399,14 +399,13 @@ def selfunban(context: CallbackContext, update: Update) -> str:
 
 
 __help__ = """
- • `/punchme`*:* punchs the user who issued the command
-
+ • `/punchme`*:* đấm vào người dùng đã ra lệnh ^^
 *Admins only:*
- • `/ban <userhandle>`*:* bans a user. (via handle, or reply)
- • `/sban <userhandle>`*:* Silently ban a user. Deletes command, Replied message and doesn't reply. (via handle, or reply)
- • `/tban <userhandle> x(m/h/d)`*:* bans a user for `x` time. (via handle, or reply). `m` = `minutes`, `h` = `hours`, `d` = `days`.
- • `/unban <userhandle>`*:* unbans a user. (via handle, or reply)
- • `/punch <userhandle>`*:* Punches a user out of the group, (via handle, or reply)
+ • `/ban <userhandle>`*:* Ban một người dùng. (qua handle, hoặc reply)
+ • `/sban <userhandle>`*:* Cấm âm thầm người dùng. Xóa lệnh, tin nhắn đã trả lời và không trả lời. (qua handle, hoặc reply)
+ • `/tban <userhandle> x(m/h/d)`*:* ban một người dùng `x` thời gian. (qua handle, hoặc reply). `m` = `phút`, `h` = `giờ`, `d` = `ngày`.
+ • `/unban <userhandle>`*:* unbans một người dùng. (qua handle, hoặc reply)
+ • `/punch <userhandle>`*:* Đấm người dùng ra khỏi nhóm, (qua handle, hoặc reply)
 """
 
 BAN_HANDLER = CommandHandler(["ban", "sban"], ban)
